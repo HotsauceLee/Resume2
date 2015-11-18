f2154a20b800f8e9dd4021d4d590ed6a294ea468
Gavin Li <gli@iparadigms.com>
2015-11-18 13:49:08 -0600

    Wrote base structs for data model. Added JSON API example.

--- resume/resume.go
+++ resume/resume.go
@@ -7,11 +7,57 @@ func init() {
 }
 
 type Resume struct {
-	name    string
-	dob     string
-	phone   string
-	email   string
-	addr    string
-	website string
-	intro   string
+	Name            string
+	Dob             string
+	Phone           string
+	Email           string
+	Addr            string
+	Website         string
+	Intro           string
+	Summary         Summary
+	WorkExperiences []WorkExperience
+	Projects        []Project
+	Edu             []Education
+}
+
+type Skill struct {
+	SLevel string
+	SType  string
+	SName  string
+}
+
+type Summary struct {
+	Background string
+	Ethic      string
+	Skills     []Skill
+}
+
+type WorkExperience struct {
+	Title    string
+	Company  string
+	Start    string
+	End      string
+	Location string
+	Projects []Project
+}
+
+type Project struct {
+	Name    string
+	Start   string
+	End     string
+	Desc    string
+	Works   []string
+	Contrib string
+	Teches  []string
+}
+
+type Education struct {
+	School      string
+	Location    string
+	Start       string
+	End         string
+	DegreeLevel string
+	Major       string
+	Gpa         float32
+	GpaMax      float32
 }
